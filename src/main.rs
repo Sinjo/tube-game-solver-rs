5e659adc76352048c8a0c74ac68fb7441c9d3a79
Chris Sinjakli <chris@sinjakli.co.uk>
2020-10-08 23:33:57 +0100

    Set up reasonable project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use libgame::game;
+
 fn main() {
+    println!("Game module returns: {}", game::do_game_stuff());
     println!("Hello, world!");
 }
---
+++ tests/game_test.rs
@@ -0,0 +1,6 @@
+use libgame::game;
+
+#[test]
+fn test_game() {
+    assert!(game::do_game_stuff());
+}
